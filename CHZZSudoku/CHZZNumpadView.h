3883ad941291d2a54e3d23d2656163a2e1f26502
Cyrus Huang <cyrushuang@cyruss-air.home>
2014-09-21 16:27:19 -0700

    add numpad view class

---
+++ CHZZSudoku/CHZZNumpadView.h
@@ -0,0 +1,13 @@
+//
+//  CHZZNumpadView.h
+//  CHZZSudoku
+//
+//  Created by Cyrus Huang on 9/21/14.
+//  Copyright (c) 2014 Cyrus Huang, Zehao Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CHZZNumpadView : NSObject
+
+@end
---
+++ CHZZSudoku/CHZZNumpadView.m
@@ -0,0 +1,13 @@
+//
+//  CHZZNumpadView.m
+//  CHZZSudoku
+//
+//  Created by Cyrus Huang on 9/21/14.
+//  Copyright (c) 2014 Cyrus Huang, Zehao Zhang. All rights reserved.
+//
+
+#import "CHZZNumpadView.h"
+
+@implementation CHZZNumpadView
+
+@end
