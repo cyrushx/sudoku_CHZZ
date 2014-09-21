83a3234b51cb13d81ca8ff899e4b097017930122
Cyrus Huang <cyrushuang@cyruss-air.home>
2014-09-21 16:25:46 -0700

    add grid model class

---
+++ CHZZSudoku/CHZZGridModel.h
@@ -0,0 +1,13 @@
+//
+//  CHZZGridModel.h
+//  CHZZSudoku
+//
+//  Created by Cyrus Huang on 9/21/14.
+//  Copyright (c) 2014 Cyrus Huang, Zehao Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CHZZGridModel : NSObject
+
+@end
---
+++ CHZZSudoku/CHZZGridModel.m
@@ -0,0 +1,13 @@
+//
+//  CHZZGridModel.m
+//  CHZZSudoku
+//
+//  Created by Cyrus Huang on 9/21/14.
+//  Copyright (c) 2014 Cyrus Huang, Zehao Zhang. All rights reserved.
+//
+
+#import "CHZZGridModel.h"
+
+@implementation CHZZGridModel
+
+@end
